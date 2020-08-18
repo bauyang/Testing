976add7c1950753e1845fff3004774e1d0d7bdd2
whatsquackin@hotmail.com <whatsquackin@hotmail.com>
2020-08-18 09:10:12 -0400

    testing

---
+++ WWC-Capstone-front-end/frontend/src/components/GeorgePashaComponent.js
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+import {
+  Card,
+  CardActionArea,
+  CardMedia,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+} from "@material-ui/core";
+import { Link } from 'react-router-dom';
+
+import { withStyles } from "@material-ui/styles";
+const useStyles = () => ({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 250,
+  },
+});
+class GeorgePashaComponent extends Component {
+  state = {};
+  render() {
+    const { classes } = this.props;
+    return (
+      <div>
+        <div className="GeorgePasha">
+          <Card className={classes.root}>
+            <CardActionArea>
+              <CardMedia
+                className={classes.media}
+                component="img"
+                height="300"
+                //image={require("../images/george.png")}
+                image={require("../images/georgepasha.png")}
+                title="GeorgePasha"
+              ></CardMedia>
+              <CardContent>
+                <Typography variant="h5" component="h2">
+                  You're like George and Pasha!
+                </Typography>
+                <Typography>
+                  While thoughtfulness is what George
+                  <br />
+                  looks for in others,
+                  <br />
+                  he doesn't think a lot about his costume choices...
+                  <br />
+                </Typography>
+                <Typography>
+                  Pasha could eat <b>a lot</b> of ice cream
+                  <br />
+                  despite his karaoke song
+                  <br />
+                  involving cheeseburgers. Suspicious...
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+              <Button component={Link} to="/quiz" color="primary">
+                Retake
+              </Button>
+            </CardActions>
+          </Card>
+        </div>
+      </div>
+    );
+  }
+}
+export default withStyles(useStyles)(GeorgePashaComponent);
